5f3c746f16ed964463517ecb5dfa786c8cae07e6
ff077 <miftahf77@gmail.com>
2019-10-13 15:22:50 +0700

    modif

--- smart_2.py
+++ smart_2.py
@@ -7,17 +7,17 @@ import paho.mqtt.client as mqtt
 from flask import Flask
 from flask_ask import Ask, statement
 
-pins={"red":3, "yellow":4, "green":17}
+pins={"first lamp":3, "second lamp":4, "third lamp":17}
 
 app=Flask(__name__)
 ask=Ask(app, '/')
 
 @ask.intent('LedIntent')
-def led(color, status):
-  if color.lower() not in pins.keys():
-     return statement("I don't have {} light".format(color))
-  GPIO.output(pins[color], GPIO.HIGH if status == "on" else GPIO.LOW)
-  return statement("Turning the {} light {}".format(color, status))
+def led(lamp, status):
+  if lamp.lower() not in pins.keys():
+     return statement("I don't have {} light".format(lamp))
+  GPIO.output(pins[lamp], GPIO.HIGH if status == "on" else GPIO.LOW)
+  return statement("Turning the {} light {}".format(lamp, status))
 
 turn_off_alarm=0
 turn_off_pir=0
